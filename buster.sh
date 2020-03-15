c16624973147ade7005109aaa7516e6e8697c975
brettjrea <6890798+brettjrea@users.noreply.github.com>
2020-03-15 15:40:55 -0400

    Update buster.sh

--- buster.sh
+++ buster.sh
@@ -4,9 +4,12 @@ export DEBIAN_FRONTEND=noninteractive
 ### Update, upgrade & clean.
 sudo apt update -y &&
 sudo apt upgrade -y &&
-sudo apt autoremove -y &&
-sudo sed -i 's/stretch/buster/g' /etc/apt/sources.list &&
+sudo apt autoremove -y
+### Switch repos from stretch to buster.
+sudo sed -i 's/stretch/buster/g' /etc/apt/sources.list
+### Update, upgrade & clean.
 sudo apt update -y &&
 sudo apt upgrade -y &&
-sudo apt autoremove -y &&
+sudo apt autoremove -y 
+### Full upgrade
 sudo apt full-upgrade
