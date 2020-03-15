7691d957ed4719a6afd10a6c9bdf7f19ccbcdf9d
brettjrea <6890798+brettjrea@users.noreply.github.com>
2020-03-15 15:34:39 -0400

    Create buster.sh

---
+++ buster.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+###Prevent interactive prompts for session.
+export DEBIAN_FRONTEND=noninteractive
+### Update, upgrade & clean.
+sudo apt update -y &&
+sudo apt upgrade -y &&
+sudo apt autoremove -y
+sudo sed -i 's/stretch/buster/g' /etc/apt/sources.list
+sudo apt update -y &&
+sudo apt upgrade -y &&
+sudo apt autoremove -y
+sudo apt full-upgrade
